d27f34d1effbfde548bbbf58de2ccfb972fd2cdf
dmachnia <01178558@pw.edu.pl>
2024-06-05 15:41:08 +0200

    Project template with working database connection

---
+++ src/test/java/warehouse/warehousemanagementsystem/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package warehouse.warehousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
