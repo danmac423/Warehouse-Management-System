6427fa35151a3ebc885744c059232920768fa900
kjurczyn <01169180@pw.edu.pl>
2024-06-12 01:24:35 +0200

    added some functions

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,3 +1,36 @@
 import requests
 import json
 
+
+
+def generate_worker_list(workers_url):
+    response = requests.get(workers_url)
+    if response.ok():
+        workers = response.json()
+        new_worker_lists = {}
+        for worker in workers:
+            new_worker_lists[worker['id']] = {
+                'name' : worker['name'],
+                'orders': [],
+                'supplies': []   
+            }
+    
+    return new_worker_lists
+
+def generate_order_list(order_url):
+    response = requests.get(order_url)
+    if response.ok():
+        orders = response.json()
+        new_order_list = {}
+        for order in orders:
+            new_order_list
+
+def generate_product_list(product_url):
+    pass
+
+def generate_category_list(category_url):
+    pass
+
+def generate_supplier_list(supplier_url):
+    pass
+            
