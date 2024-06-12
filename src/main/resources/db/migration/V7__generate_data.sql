cf304c73728ff4746e12d9d43cedb30b0aef0485
dmachnia <01178558@pw.edu.pl>
2024-06-12 23:14:26 +0200

    Hash workers passwords

--- src/main/resources/db/migration/V7__generate_data.sql
+++ src/main/resources/db/migration/V7__generate_data.sql
@@ -91,19 +91,19 @@ BEGIN
 	RETURNING id INTO addr_id9;
 
 	INSERT INTO workers (username, password, name, last_name, role)
-    VALUES ('jdoe', 'passwordJohn','John', 'Doe', 'WORKER')
+    VALUES ('jdoe', '$2a$10$FCe77RclwMpAgjR5hdr5Eu1J2yi/tH65azbwzQbLg4c3//ctmPeSG','John', 'Doe', 'WORKER')
     RETURNING id INTO work_id1;
 
     INSERT INTO workers (username, password, name, last_name, role)
-    VALUES ('jsmith', 'kitty123','Jane', 'Smith', 'WORKER')
+    VALUES ('jsmith', '$2a$10$lKo.7FvgU14cXA7uS1Ox.eBk3eOjpAGybliUWWKv25o7b1K3CmHyy','Jane', 'Smith', 'WORKER')
     RETURNING id INTO work_id2;
 
     INSERT INTO workers (username, password, name, last_name, role)
-    VALUES ('mjohnson', 'jordan95','Michael', 'Johnson', 'WORKER')
+    VALUES ('mjohnson', '$2a$10$zYLkMZQWvm.fcHCbF8ztVOtQz55Sx2Fyr7BlsmComBa0HVIKx1SZa','Michael', 'Johnson', 'WORKER')
     RETURNING id INTO work_id3;
 
     INSERT INTO workers (username, password, name, last_name, role)
-    VALUES ('ewilliams', 'sunny67', 'Emily', 'Williams', 'WORKER')
+    VALUES ('ewilliams', '$2a$10$ERIS98Fhq6Z.0VGIwr9oNeZcAge.NZHZppDJsPmNllcoS3HwOjRCS', 'Emily', 'Williams', 'WORKER')
     RETURNING id INTO work_id4;
 
     INSERT INTO workers (username, password, name, last_name, role)
