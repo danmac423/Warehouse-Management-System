a2d7052f82dc053a7ecfa9fdb4dd9e70776e1e96
kjurczyn <01169180@pw.edu.pl>
2024-06-13 19:21:55 +0200

    removed unnecessary import

--- src/main/java/warehouse/warehousemanagementsystem/ordersHistory/OrdersHistoryDao.java
+++ src/main/java/warehouse/warehousemanagementsystem/ordersHistory/OrdersHistoryDao.java
@@ -3,7 +3,6 @@ package warehouse.warehousemanagementsystem.ordersHistory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import warehouse.warehousemanagementsystem.order.Order;
 
 import java.util.Date;
 import java.util.List;
