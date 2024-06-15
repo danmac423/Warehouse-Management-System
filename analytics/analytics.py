50deeadfb47d8eb790f16476051793308ef72445
kjurczyn <01169180@pw.edu.pl>
2024-06-15 02:34:54 +0200

    added plotting by month

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -3,6 +3,7 @@ import requests
 from matplotlib import pyplot as plt
 import numpy as np
 from datetime import datetime as dt
+from calendar import monthrange
 
 
 
@@ -42,8 +43,8 @@ from datetime import datetime as dt
 def _get_order_num_by_day(endpoint_url:str, worker_id:str, date_min:str=None, date_max:str=None) -> dict:
     response = None
     if date_min and date_max:
-        date_min = dt.isoformat(dt.strptime(date_min,  '%Y-%m-%d'), timespec='microseconds')
-        date_max = dt.isoformat(dt.strptime(date_max,  '%Y-%m-%d').replace(hour=23, minute=59, second=59), timespec='microseconds')
+        date_min = dt.strptime(date_min,  '%Y-%m-%d').isoformat(timespec='microseconds')
+        date_max = dt.strptime(date_max,  '%Y-%m-%d').replace(hour=23, minute=59, second=59).isoformat(timespec='microseconds')
         response = requests.get(endpoint_url + worker_id + '/' + date_min +'+00:00' + '/' + date_max + '+00:00')
     else:
         response = requests.get(endpoint_url+worker_id)
@@ -52,13 +53,36 @@ def _get_order_num_by_day(endpoint_url:str, worker_id:str, date_min:str=None, da
         orders = response.json()
         order_num_by_day = {}
         for order in orders:
-            print(order)
             order_date = np.datetime64(order['dateProcessed'][:10])
             order_num_by_day[order_date] = order_num_by_day.get(order_date, 0) + 1
 
         return order_num_by_day
     
     raise(Exception(response.text))
+
+def _get_order_num_by_month(endpoint_url:str, worker_id:str, date_min:str=None, date_max:str=None) -> dict:
+    response = None
+    if date_min and date_max:
+        date_min = dt.strptime(date_min,  '%Y-%m').isoformat(timespec='microseconds')
+        date_max:dt = dt.strptime(date_max,  '%Y-%m')
+        num_days = monthrange(date_max.year, date_max.month)[1]
+        date_max = date_max.replace(day=num_days, hour=23, minute=59, second=59).isoformat(timespec='microseconds')
+        response = requests.get(endpoint_url + worker_id + '/' + date_min +'+00:00' + '/' + date_max + '+00:00')
+    else:
+        response = requests.get(endpoint_url+worker_id)
+
+    if response.ok:
+        orders = response.json()
+        order_num_by_month = {}
+        for order in orders:
+            order_month = np.datetime64(order['dateProcessed'][:7])
+            order_num_by_month[order_month] = order_num_by_month.get(order_month, 0) + 1
+        
+        return order_num_by_month
+
+    raise(Exception(response.text))
+
+        
 
 def _get_worker_name(endpoint_url:str, worker_id:str) -> Tuple[str, str]:
     response = requests.get(endpoint_url+worker_id)
@@ -96,38 +120,56 @@ def _get_categories_stats(orders_endpoint_url:str, product_list_url:str) -> dict
     
     raise(Exception(response.text))
 
-# TODO rozwiązać anomalię z 13.06.2024
-
-def graph_worker_unloaded_orders_per_day(endpoint_url_orders:str, endpoint_url_workers:str, worker_id:str, ax:plt.Axes, date_min:str=None, date_max:str=None, graph_type:str='line') -> int :
-    order_num_by_day = _get_order_num_by_day(endpoint_url_orders, worker_id, date_min=date_min, date_max=date_max)
-    worker_name, worker_surname = _get_worker_name(endpoint_url_workers, worker_id)
-    plot_func = None
-    if graph_type == 'line':
-        ax.plot(order_num_by_day.keys(), list(order_num_by_day.values()), '-o', label=worker_name + ' ' + worker_surname)
-    else:
-        ax.bar(order_num_by_day.keys(), list(order_num_by_day.values()), label=worker_name + ' ' + worker_surname)
-
-    max_val = 0
-    min_date = None
-    max_date = None
-    try:
-        max_val = max(list(order_num_by_day.values()))
-        min_date = min(list(order_num_by_day.keys()))
-        max_date = max(list(order_num_by_day.keys()))
-    except:
-        if len(order_num_by_day.keys()) == 1:
-            min_date = order_num_by_day.keys[0]
-            max_date = order_num_by_day.keys[0] 
-        else:
-            min_date = np.datetime64(dt.today().isoformat())
-            max_date = np.datetime64(dt.today().isoformat())
-
-    return max_val, min_date, max_date   
+def graph_worker_unloaded_orders(endpoint_url_orders:str, endpoint_url_workers:str, worker_id_list:List[str], per:str, ax:plt.Axes, 
+                                         date_min:str=None, date_max:str=None, graph_type:str='line') -> None :
     
+    get_func = None
+    if per == 'month':
+        get_func = _get_order_num_by_month
+        ax.set_xlabel('Month')
+        if date_min and date_max:
+            date_min_datetime = np.datetime64(date_min)
+            date_max_datetime = np.datetime64(date_max)
 
-def graph_worker_unloaded_orders(endpoint_url:str, worker_id_list:List[str], ax:plt.Axes, graph_type:str=None):
-    pass
+            one_month = np.timedelta64(1, 'M')
+            ax.set_xticks(np.arange(date_min_datetime-one_month, date_max_datetime+one_month, one_month))
 
+    else:
+        get_func = _get_order_num_by_day
+        ax.set_xlabel('Date')
+        if date_min and date_max:
+            date_min_datetime = np.datetime64(date_min)
+            date_max_datetime = np.datetime64(date_max)
+
+            one_day = np.timedelta64(1, 'D')
+            ax.set_xticks(np.arange(date_min_datetime-one_day, date_max_datetime+one_day, one_day))
+
+    ax.grid(True)
+    ax.set_ylabel('No. of unpacked orders')
+    
+    max_num_orders = 0
+    for worker_id in worker_id_list:
+        worker_name, worker_surname = _get_worker_name(endpoint_url_workers, worker_id)
+
+        order_nums = get_func(endpoint_url_orders, worker_id, date_min=date_min, date_max=date_max)
+
+        x = list(order_nums.keys())
+        y = list(order_nums.values())
+
+        if graph_type == 'line':
+            ax.plot(x, y, '-o', label=worker_name + ' ' + worker_surname)
+        else:
+            ax.bar(x, y, label=worker_name + ' ' + worker_surname)
+
+        try:
+            max_num_orders = max(max(y), max_num_orders)
+        except:
+            pass
+    
+    ax.set_yticks(np.arange(0, max_num_orders+1, 1))
+    ax.legend()
+
+    return 
 
 
 def graph_orders_by_category(orders_endpoint_url:str, product_list_url:str, ax:plt.Axes) -> bool:
--- analytics/test.py
+++ analytics/test.py
@@ -7,23 +7,16 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(1,1)
     
 
-    order_num_max, date_min, date_max = analytics.graph_worker_unloaded_orders_per_day('http://localhost:8080/api/ordersHistory/worker/', 
+    analytics.graph_worker_unloaded_orders('http://localhost:8080/api/ordersHistory/worker/', 
                                                                    'http://localhost:8080/api/workers/', 
-                                                                   '1', 
+                                                                   ['1', '2'], 
+                                                                   'month',
                                                                    ax, 
-                                                                   date_min='2024-06-13', 
-                                                                   date_max='2024-06-13',
+                                                                  #  date_min='2024-06', 
+                                                                  #  date_max='2024-06',
                                                                 graph_type='line'
                                                                    )
-    ax.set_yticks(np.arange(0, order_num_max+1, 1))
-    one_day = np.timedelta64(1, 'D')
-    if date_min == date_max:
-        ax.set_xticks([date_min])
-    else:
-        ax.set_xticks(np.arange(date_min, date_max+one_day, one_day))
-    ax.xaxis_date()
-    ax.grid(True)
-    ax.legend()
+
 
     fig, ax = plt.subplots(1,1)
     analytics.graph_orders_by_category('http://localhost:8080/api/ordersHistory', 'http://localhost:8080/api/products/orderHistory/', ax)
