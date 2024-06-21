fe10f87f3ec8d7fa1d9e4ae6be950090b887b94a
Krzysztof Golcz <krzysgolcz@gmail.com>
2024-06-22 00:49:19 +0200

    Added OrderHistoryView and the implementation

--- src/main/java/warehouse/warehousemanagementsystem/ordersHistory/OrdersHistoryDao.java
+++ src/main/java/warehouse/warehousemanagementsystem/ordersHistory/OrdersHistoryDao.java
@@ -3,6 +3,10 @@ package warehouse.warehousemanagementsystem.ordersHistory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import warehouse.warehousemanagementsystem.orderView.OrderView;
+import warehouse.warehousemanagementsystem.orderView.OrderViewGeneral;
+import warehouse.warehousemanagementsystem.orderView.OrderViewGeneralMapper;
+import warehouse.warehousemanagementsystem.orderView.OrderViewMapper;
 
 import java.util.List;
 
@@ -50,5 +54,190 @@ public class OrdersHistoryDao {
                 new OrdersHistoryMapper(),
                 toSearch
         );
+    }
+
+    // zapytac czy musze robic kolejny view
+    public List<OrderViewGeneral> getAllOrdersViews() {
+        var sql = """
+                SELECT
+                    orders.id,
+                    customer.email,
+                    orders.date_processed,
+                    orders.status,
+                    orders.date_received,
+                    workers.username,
+                    COALESCE(SUM(product.price * products_orders.amount), 0) AS total_price
+                FROM
+                    orders
+                LEFT JOIN
+                    products_orders ON orders.id = products_orders.order_id
+                LEFT JOIN
+                    products product ON products_orders.product_id = product.id
+                LEFT JOIN
+                    customers customer ON orders.customer_id = customer.id
+                LEFT JOIN
+                    workers ON orders.worker_id = workers.id
+                GROUP BY
+                    orders.id, orders.customer_id, customer.name, customer.last_name, customer.email,
+                    orders.date_processed, orders.worker_id, workers.username, orders.status, orders.date_received
+                ORDER BY
+                    orders.id;
+                """;
+        return jdbcTemplate.query(
+                sql,
+                new OrderViewGeneralMapper()
+        );
+    }
+
+    public List<OrdersHistoryView> getOrdersHistViewsByWorkerUsernameSubstring(String usernameSubstring) {
+        var sql = """
+                SELECT
+                    orders_history.id,
+                    orders_history.customer_id,
+                    customer.name AS customer_name,
+                    customer.last_name,
+                    customer.email,
+                    orders_history.date_processed,
+                    orders_history.worker_id,
+                	workers.username,
+                    orders_history.date_received,
+                    COALESCE(SUM(product.price * products_orders_history.amount), 0) AS total_price
+                FROM
+                    orders_history
+                LEFT JOIN
+                    products_orders_history ON orders_history.id = products_orders_history.order_id
+                LEFT JOIN
+                    products product ON products_orders_history.product_id = product.id
+                LEFT JOIN
+                    customers customer ON orders_history.customer_id = customer.id
+                LEFT JOIN
+                    workers ON orders_history.worker_id = workers.id
+                WHERE worker_id IN (SELECT id FROM workers WHERE LOWER(workers.username) like LOWER((?)))
+                GROUP BY
+                    orders_history.id, orders_history.customer_id, customer.name, customer.last_name, customer.email,
+                    orders_history.date_processed, orders_history.worker_id, workers.username, orders_history.date_received
+                ORDER BY
+                    orders_history.id;
+                """;
+        return jdbcTemplate.query(
+                sql,
+                new OrdersHistoryViewMapper(),
+                "%" + usernameSubstring + "%"
+        );
+    }
+
+    public List<OrdersHistoryView> getOrdersHistViewsByCustomerEmailSubstring(String emailSubstring) {
+        var sql = """
+                SELECT
+                    orders_history.id,
+                    orders_history.customer_id,
+                    customer.name AS customer_name,
+                    customer.last_name,
+                    customer.email,
+                    orders_history.date_processed,
+                    orders_history.worker_id,
+                	workers.username,
+                    orders_history.date_received,
+                    COALESCE(SUM(product.price * products_orders_history.amount), 0) AS total_price
+                FROM
+                    orders_history
+                LEFT JOIN
+                    products_orders_history ON orders_history.id = products_orders_history.order_id
+                LEFT JOIN
+                    products product ON products_orders_history.product_id = product.id
+                LEFT JOIN
+                    customers customer ON orders_history.customer_id = customer.id
+                LEFT JOIN
+                    workers ON orders_history.worker_id = workers.id
+                WHERE customer_id IN (SELECT id FROM customers WHERE LOWER(customers.email) like LOWER((?)))
+                GROUP BY
+                    orders_history.id, orders_history.customer_id, customer.name, customer.last_name, customer.email,
+                    orders_history.date_processed, orders_history.worker_id, workers.username, orders_history.date_received
+                ORDER BY
+                    orders_history.id;
+                """;
+        return jdbcTemplate.query(
+                sql,
+                new OrdersHistoryViewMapper(),
+                "%" + emailSubstring + "%"
+        );
+    }
+
+    public List<OrdersHistoryView> getOrdersHistViewsByCustomerEmailWorkerUsernameSubstring(String emailSubstring, String usernameSubstring) {
+        var sql = """
+                SELECT
+                    orders_history.id,
+                    orders_history.customer_id,
+                    customer.name AS customer_name,
+                    customer.last_name,
+                    customer.email,
+                    orders_history.date_processed,
+                    orders_history.worker_id,
+                	workers.username,
+                    orders_history.date_received,
+                    COALESCE(SUM(product.price * products_orders_history.amount), 0) AS total_price
+                FROM
+                    orders_history
+                LEFT JOIN
+                    products_orders_history ON orders_history.id = products_orders_history.order_id
+                LEFT JOIN
+                    products product ON products_orders_history.product_id = product.id
+                LEFT JOIN
+                    customers customer ON orders_history.customer_id = customer.id
+                LEFT JOIN
+                    workers ON orders_history.worker_id = workers.id
+                WHERE customer_id IN (SELECT id FROM customers WHERE LOWER(customers.email) like LOWER((?))) AND
+                    worker_id IN (SELECT id FROM workers WHERE LOWER(workers.username) like LOWER((?)))
+                GROUP BY
+                    orders_history.id, orders_history.customer_id, customer.name, customer.last_name, customer.email,
+                    orders_history.date_processed, orders_history.worker_id, workers.username, orders_history.date_received
+                ORDER BY
+                    orders_history.id;
+                """;
+        ;
+        return jdbcTemplate.query(
+                sql,
+                new OrdersHistoryViewMapper(),
+                "%" + emailSubstring + "%",
+                "%" + usernameSubstring + "%"
+        );
+    }
+
+    public List<OrderView> getOrdersHistViewsByWorker(Long workerId) {
+        var sql = """
+                SELECT
+                    orders_history.id,
+                    orders_history.customer_id,
+                    customer.name AS customer_name,
+                    customer.last_name,
+                    customer.email,
+                    orders_history.date_processed,
+                    orders_history.worker_id,
+                	workers.username,
+                    orders_history.date_received,
+                    COALESCE(SUM(product.price * products_orders_history.amount), 0) AS total_price
+                FROM
+                    orders_history
+                LEFT JOIN
+                    products_orders_history ON orders_history.id = products_orders_history.order_id
+                LEFT JOIN
+                    products product ON products_orders_history.product_id = product.id
+                LEFT JOIN
+                    customers customer ON orders_history.customer_id = customer.id
+                LEFT JOIN
+                    workers ON orders_history.worker_id = workers.id
+                WHERE worker_id = ?
+                GROUP BY
+                    orders_history.id, orders_history.customer_id, customer.name, customer.last_name, customer.email,
+                    orders_history.date_processed, orders_history.worker_id, workers.username, orders_history.date_received
+                ORDER BY
+                    orders_history.id;
+                """;
+        return jdbcTemplate.query(
+                sql,
+                new OrderViewMapper(),
+                workerId
+        );
+
     }
 }
