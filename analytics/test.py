d6e0ff5ea869a35589fdbf896811c27502b02880
kjurczyn <01169180@pw.edu.pl>
2024-06-24 03:31:29 +0200

    fixed formatting in test

--- analytics/test.py
+++ analytics/test.py
@@ -20,10 +20,6 @@ if __name__ == "__main__":
                                                                    )
    
    analytics.dump_orders_data_to_csv('data.csv', 'processed_times', 'http://localhost:8080/api/ordersHistory/worker/','http://localhost:8080/api/workers/',
-                                      [1, 2], 
-                                                                   'day',
-                                                                   date_min='2024-06-01', 
-                                                                   date_max='2024-06-30',
-                                                      )
+                                      [1, 2], 'day', date_min='2024-06-01', date_max='2024-06-30',)
 
    #  analytics.graph_orders_by_category('http://localhost:8080/api/ordersHistory', 'http://localhost:8080/api/products/orderHistory/', ax)
